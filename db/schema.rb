4af98886d9f62d4270ca147cae7144176c011b7c
mgmilton <matt.milton@gmail.com>
2018-06-01 10:23:21 -0600

    adds comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180601154421) do
+ActiveRecord::Schema.define(version: 20180601160207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_comments", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.bigint "comment_id"
+    t.index ["blog_id"], name: "index_blog_comments_on_blog_id"
+    t.index ["comment_id"], name: "index_blog_comments_on_comment_id"
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "article"
     t.bigint "user_id"
+    t.bigint "{:foreign_key=>true}_id"
     t.index ["user_id"], name: "index_blogs_on_user_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_blogs_on_{:foreign_key=>true}_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "blog_id"
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,5 +43,8 @@ ActiveRecord::Schema.define(version: 20180601154421) do
     t.string "password_digest"
   end
 
+  add_foreign_key "blog_comments", "blogs"
+  add_foreign_key "blog_comments", "comments"
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
 end
