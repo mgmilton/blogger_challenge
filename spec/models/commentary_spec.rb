526fc77bb25da51c2f28af92fb175aaf07e27fd9
mgmilton <matt.milton@gmail.com>
2018-06-01 10:46:41 -0600

    renames comment model test to commentary model

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
-describe Comments, type: :model do
+describe Commentary, type: :model do
   describe "validations" do
     it "presence of text required" do
       user_1 = User.create(name: "user_one", email: "user_one@email.com", password: "test_one")
       blog_1 = user_1.blogs.create(title: "first_blog", article: "hello world")
 
-      comment_1 = blog_1.comments.create(text: "This is a test comment")
+      comment_1 = blog_1.commentaries.create(text: "This is a test comment")
+      comment_2 = blog_1.commentaries.create(text: "")
 
       expect(comment_1).to be_valid
       expect(comment_2).to_not be_valid
